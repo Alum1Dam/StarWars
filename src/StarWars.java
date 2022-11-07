fe64e2d88a3d7624a3037c4edb457c95176c58c9
Alumno1Dam <jeagraver@alu.edu.gva.es>
2022-11-07 16:02:56 +0100

    Hacer un Commit como Práctica

---
+++ src/StarWars.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class StarWars {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== STAR WARS CÓDIGO SECRETOS ===");
+        System.out.println("Hace mucho tiempo, en una galaxia muy, muy lejana... La Princesa Leia, Luke \n" +
+                "Skywalker, Han Solo, Chewbacca, C3PO y R2D2 viajan en una nave imperial robada \n" +
+                "en una misión secreta para infiltrarse en otra estrella de la muerte que el imperio \n" +
+                "está construyendo para destruirla. (Presiona Intro para continuar)");
+        sc.next();
+        //Haz
+        do {
+
+            int S1 = (int) (Math.random() * (1 + 10));
+            int S2 = (int) (Math.random() * (20 + 30));
+
+            for () {
+
+            }
+
+        }/*Mientras*/ while ();
+
+        sc.close();
+    }
+}
